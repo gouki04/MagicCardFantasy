3933c442bbc77d073acfe7c1151746d3b767449e
gouki04 <gouki04@126.com>
2014-01-27 00:00:33 +0800

    改为使用之前的main.lua

--- MagicCardFantasy/scripts/main.lua
+++ MagicCardFantasy/scripts/main.lua
@@ -1,7 +1,29 @@
+--[[
+	@brief scripts/main.lua
+]]
+
 require 'framework.init'
 require 'framework.functions'
 
+require 'scripts/utility/utility'
+require 'scripts/model/hero/hero'
+require 'scripts/model/game'
+
 local CURRENT_MODULE_NAME = ...
 
-local t = require 'test.test1'
-t()
+local hero1 = Hero:new()
+hero1:init(10, 'a')
+hero1:addCardToDeck(1, 3)
+hero1:addCardToDeck(2, 5)
+hero1:addCardToDeck(3, 7)
+hero1:addCardToDeck(1, 9)
+hero1:addCardToDeck(2, 1)
+
+local hero2 = Hero:new()
+hero2:init(8, 'b')
+hero2:addCardToDeck(2, 10)
+hero2:addCardToDeck(2, 10)
+hero2:addCardToDeck(3, 10)
+--hero2:addCardToDeck(1, 3)
+
+Game.start(hero1, hero2)
